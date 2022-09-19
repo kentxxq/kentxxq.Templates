77df7c1a098d3441ed86e786ea16abb04fdcda94
kentxxq <admin@kentxxq.com>
2022-09-20 02:38:33 +0800

    style: 抑制不必要的告警

--- src/kentxxq.Templates.Aspnetcore.DBManager/Program.cs
+++ src/kentxxq.Templates.Aspnetcore.DBManager/Program.cs
@@ -12,9 +12,12 @@ var db = new SqlSugarClient(new ConnectionConfig
 
 db.DbMaintenance.CreateDatabase();
 
+#pragma warning disable CS8602 // 解引用可能出现空引用。
 var types = Assembly
-    .LoadFrom("kentxxq.Templates.Aspnetcore.dll") //如果 .dll报错，可以换成 xxx.exe 有些生成的是exe 
-    .GetTypes().Where(it => it.FullName.StartsWith("kentxxq.Templates.Aspnetcore")) //命名空间过滤，当然你也可以写其他条件过滤
+    .LoadFrom("kentxxq.Templates.Aspnetcore.DB.dll") //如果 .dll报错，可以换成 xxx.exe 有些生成的是exe 
+    .GetTypes()
+    .Where(it => it.FullName.StartsWith("kentxxq.Templates.Aspnetcore")) //命名空间过滤，当然你也可以写其他条件过滤
     .ToArray(); //断点调试一下是不是需要的Type，不是需要的在进行过滤
+#pragma warning restore CS8602 // 解引用可能出现空引用。
 
 db.CodeFirst.SetStringDefaultLength(200).InitTables(types); //根据types创建表
