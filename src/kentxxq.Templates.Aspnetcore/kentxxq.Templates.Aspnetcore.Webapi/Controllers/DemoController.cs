45b6ce9fdab80124d9749a6573a7d2bf8b504d07
kentxxq <805429509@qq.com>
2022-11-16 16:58:52 +0800

    style: 优化格式

--- src/kentxxq.Templates.Aspnetcore/kentxxq.Templates.Aspnetcore.Webapi/Controllers/DemoController.cs
+++ src/kentxxq.Templates.Aspnetcore/kentxxq.Templates.Aspnetcore.Webapi/Controllers/DemoController.cs
@@ -1,10 +1,11 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System.Globalization;
 using IdentityModel;
 using kentxxq.Templates.Aspnetcore.Webapi.Common;
 using kentxxq.Templates.Aspnetcore.Webapi.Common.Response;
 using kentxxq.Templates.Aspnetcore.Webapi.Services;
 using kentxxq.Templates.Aspnetcore.Webapi.Services.Tools;
 using kentxxq.Templates.Aspnetcore.Webapi.SO.Demo;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 #if (EnableDB)
 using kentxxq.Templates.Aspnetcore.Webapi.Services.UserInfo;
@@ -19,8 +20,6 @@ using Nacos.AspNetCore.V2;
 using Nacos.V2;
 using Microsoft.Extensions.Options;
 #endif
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.RateLimiting;
 
 namespace kentxxq.Templates.Aspnetcore.Webapi.Controllers;
 
@@ -33,27 +32,15 @@ namespace kentxxq.Templates.Aspnetcore.Webapi.Controllers;
 public class DemoController : ControllerBase
 {
     private readonly IDemoService _demoService;
-    private readonly JWTService _jwtService;
     private readonly IIpService _ipService;
-#if (EnableQuartz)
-    private readonly ISchedulerFactory _schedulerFactory;
-#endif
-#if (EnableDB)
-    private readonly IUserService _userService;
-#endif
-#if (EnableNacos)
-    private readonly NacosSettings _appSettings;
-    private readonly NacosSettings _mAppSettings;
-    private readonly NacosAspNetOptions _nacosAspNetOptions;
-    private readonly INacosNamingService _nacosNamingService;
-    private readonly NacosSettings _sAppSettings;
-#endif
+    private readonly JWTService _jwtService;
 
+    #region 依赖注入
 
     /// <inheritdoc />
     public DemoController(
         IDemoService demoService
-        ,JWTService jwtService
+        , JWTService jwtService
         , IIpService ipService
 #if (EnableQuartz)
         , ISchedulerFactory schedulerFactory
@@ -88,6 +75,8 @@ public class DemoController : ControllerBase
 #endif
     }
 
+    #endregion
+
     /// <summary>
     /// 获取demo数据
     /// </summary>
@@ -97,7 +86,6 @@ public class DemoController : ControllerBase
     {
         return ResultModel<string>.Ok(await _demoService.GetData());
     }
-
 
     /// <summary>
     /// 获取ip数据
@@ -111,7 +99,123 @@ public class DemoController : ControllerBase
         return ResultModel<IpSO>.Ok(result);
     }
 
+    /// <summary>
+    /// 客户端缓存10s，不会发送新的请求过来
+    /// location.client代表只能在浏览器存储，不能由cdn，nginx等中间层缓存. any代表中间层也可以缓存。none可以理解为代表no-cache，仅中间件缓存。no-store单独设置bool值，直接不缓存
+    /// https://learn.microsoft.com/zh-cn/aspnet/core/performance/caching/response?view=aspnetcore-6.0#http-based-response-caching
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Client)]
+    public async Task<ResultModel<string>> TestClientCache()
+    {
+        await Task.Delay(2000);
+        return ResultModel<string>.Ok("1");
+    }
+
+    /// <summary>
+    /// 测试内存缓存
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<ResultModel<string>> TestLocalCacheData()
+    {
+        var data = await _demoService.GetLocalCacheData();
+        return ResultModel<string>.Ok(data.ToString());
+    }
+
+
+    /// <summary>
+    /// 刷新token
+    /// </summary>
+    /// <returns>新token</returns>
+    [Authorize]
+    [HttpGet]
+    public ResultModel<string> RefreshToken()
+    {
+        //var token = Request.Headers.Authorization.ToString()[7..];
+        //var jwtSecurityToken = new JwtSecurityToken(token);
+        //var schemaName = jwtSecurityToken.Subject;
+        //var uid = int.Parse(jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.Id).Value);
+        //var userName = jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.NickName).Value;
+        //var roles = jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.Role).Value.Split(",");
+        //下面这种方式，来自不同的claim枚举类，取值不统一，但是controller自带。
+        var schemaName = User.Claims.First(u => u.Type == ClaimTypes.NameIdentifier).Value;
+        var uid = int.Parse(User.Claims.First(u => u.Type == JwtClaimTypes.Id).Value);
+        var userName = User.Claims.First(u => u.Type == JwtClaimTypes.NickName).Value;
+        var roles = User.Claims.First(u => u.Type == ClaimTypes.Role).Value.Split(",");
+        return ResultModel<string>.Ok(_jwtService.GetToken(uid, userName, roles, schemaName));
+    }
+
+    #region 限速
+
+    /// <summary>
+    /// 限速接口
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public ResultModel<string> LimitApi()
+    {
+        return ResultModel<string>.Ok("LimitApi");
+    }
+
+    #endregion
+
+#if (EnableRedis)
+    /// <summary>
+    /// 测试redis缓存
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<ResultModel<string>> TestRedisCacheData()
+    {
+        var data = await _demoService.GetRedisCacheData();
+        return ResultModel<string>.Ok(data.ToString(CultureInfo.InvariantCulture));
+    }
+#endif
+
+    #region 授权、拦截
+
+    /// <summary>
+    /// 不需要授权，只要token正确即可
+    /// </summary>
+    /// <returns></returns>
+    [Authorize]
+    [HttpGet]
+    public ResultModel<string> GetAuthData()
+    {
+        return ResultModel<string>.Ok("AuthData");
+    }
+
+    /// <summary>
+    /// 角色是admin或者superadmin
+    /// </summary>
+    /// <returns></returns>
+    [Authorize(AuthenticationSchemes = "Bearer", Roles = "admin,superadmin")]
+    [HttpGet]
+    public ResultModel<string> GetAdminData()
+    {
+        return ResultModel<string>.Ok("AdminData");
+    }
+
+    /// <summary>
+    /// 指定is_allow策略，自定义策略
+    /// </summary>
+    /// <returns></returns>
+    [Authorize(AuthenticationSchemes = "Bearer", Policy = "is_allow")]
+    [HttpGet]
+    public ResultModel<string> GetAllowData()
+    {
+        return ResultModel<string>.Ok("AllowData");
+    }
+
+    #endregion
+
 #if (EnableQuartz)
+    private readonly ISchedulerFactory _schedulerFactory;
+
+    #region 定时任务
+
     /// <summary>
     /// 获取scheduler状态
     /// </summary>
@@ -147,9 +251,17 @@ public class DemoController : ControllerBase
         };
         return ResultModel<SchedulerStatusSO>.Ok(data);
     }
+
+    #endregion
+
 #endif
 
+
 #if (EnableDB)
+    private readonly IUserService _userService;
+
+    #region 数据库相关
+
     /// <summary>
     /// 用户登录
     /// </summary>
@@ -162,14 +274,11 @@ public class DemoController : ControllerBase
         var user = await _userService.Login(username, password);
         if (user is not null)
         {
-            var token = _jwtService.GetToken(user.Id, user.Username, new List<string>{ "admin" });
+            var token = _jwtService.GetToken(user.Id, user.Username, new List<string> { "admin" });
             return ResultModel<string>.Ok(token);
         }
-        else
-        {
-            return ResultModel<string>.Error("登录失败，用户名或密码错误","");
-        }
-        
+
+        return ResultModel<string>.Error("登录失败，用户名或密码错误", "");
     }
 
     /// <summary>
@@ -184,10 +293,21 @@ public class DemoController : ControllerBase
         var result = data.Select(a => Mapper.AddressToAddressSO(a));
         return ResultModel<IEnumerable<AddressSO>>.Ok(result);
     }
+
+    #endregion
+
 #endif
 
-
 #if (EnableNacos)
+
+    #region nacos配置
+
+    private readonly NacosSettings _appSettings;
+    private readonly NacosSettings _mAppSettings;
+    private readonly NacosAspNetOptions _nacosAspNetOptions;
+    private readonly INacosNamingService _nacosNamingService;
+    private readonly NacosSettings _sAppSettings;
+
     /// <summary>
     /// 获取注册上去的主机连接信息
     /// </summary>
@@ -195,7 +315,8 @@ public class DemoController : ControllerBase
     [HttpGet]
     public ResultModel<string> GetHost()
     {
-        var instance = _nacosNamingService.SelectOneHealthyInstance("kentxxq.Templates.Aspnetcore", "dev_demo_group").GetAwaiter()
+        var instance = _nacosNamingService.SelectOneHealthyInstance("kentxxq.Templates.Aspnetcore", "dev_demo_group")
+            .GetAwaiter()
             .GetResult();
         var host = $"{instance.Ip}:{instance.Port}";
         return ResultModel<string>.Ok(host);
@@ -221,109 +342,8 @@ public class DemoController : ControllerBase
     {
         return ResultModel<NacosAspNetOptions>.Ok(_nacosAspNetOptions);
     }
+
+    #endregion
+
 #endif
-
-
-    /// <summary>
-    /// 客户端缓存10s，不会发送新的请求过来
-    /// location.client代表只能在浏览器存储，不能由cdn，nginx等中间层缓存. any代表中间层也可以缓存。none可以理解为代表no-cache，仅中间件缓存。no-store单独设置bool值，直接不缓存
-    /// https://learn.microsoft.com/zh-cn/aspnet/core/performance/caching/response?view=aspnetcore-6.0#http-based-response-caching
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Client)]
-    public async Task<ResultModel<string>> TestClientCache()
-    {
-        await Task.Delay(2000);
-        return ResultModel<string>.Ok("1");
-    }
-
-    /// <summary>
-    /// 测试内存缓存
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    public async Task<ResultModel<string>> TestLocalCacheData()
-    {
-        var data = await _demoService.GetLocalCacheData();
-        return ResultModel<string>.Ok(data.ToString());
-    }
-
-#if (EnableRedis)
-    /// <summary>
-    /// 测试redis缓存
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    public async Task<ResultModel<string>> TestRedisCacheData()
-    {
-        var data = await _demoService.GetRedisCacheData();
-        return ResultModel<string>.Ok(data.ToString());
-    }
-#endif
-
-    /// <summary>
-    /// 刷新token
-    /// </summary>
-    /// <returns>新token</returns>
-    [Authorize]
-    [HttpGet]
-    public ResultModel<string> RefreshToken()
-    {
-        //var token = Request.Headers.Authorization.ToString()[7..];
-        //var jwtSecurityToken = new JwtSecurityToken(token);
-        //var schemaName = jwtSecurityToken.Subject;
-        //var uid = int.Parse(jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.Id).Value);
-        //var userName = jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.NickName).Value;
-        //var roles = jwtSecurityToken.Claims.First(u => u.Type == JwtClaimTypes.Role).Value.Split(",");
-        //下面这种方式，来自不同的claim枚举类，取值不统一，但是controller自带。
-        var schemaName = User.Claims.First(u => u.Type == ClaimTypes.NameIdentifier).Value;
-        var uid = int.Parse(User.Claims.First(u => u.Type == JwtClaimTypes.Id).Value);
-        var userName = User.Claims.First(u => u.Type == JwtClaimTypes.NickName).Value;
-        var roles = User.Claims.First(u => u.Type == ClaimTypes.Role).Value.Split(",");
-        return ResultModel<string>.Ok(_jwtService.GetToken(uid, userName, roles, schemaName));
-    }
-
-    /// <summary>
-    /// 不需要授权，只要token正确即可
-    /// </summary>
-    /// <returns></returns>
-    [Authorize]
-    [HttpGet]
-    public ResultModel<string> GetAuthData()
-    {
-        return ResultModel<string>.Ok("AuthData");
-    }
-
-    /// <summary>
-    /// 角色是admin或者superadmin
-    /// </summary>
-    /// <returns></returns>
-    [Authorize(AuthenticationSchemes ="Bearer",Roles ="admin,superadmin")]
-    [HttpGet]
-    public ResultModel<string> GetAdminData()
-    {
-        return ResultModel<string>.Ok("AdminData");
-    }
-    
-    /// <summary>
-    /// 指定is_allow策略，自定义策略
-    /// </summary>
-    /// <returns></returns>
-    [Authorize(AuthenticationSchemes ="Bearer",Policy ="is_allow")]
-    [HttpGet]
-    public ResultModel<string> GetAllowData()
-    {
-        return ResultModel<string>.Ok("AllowData");
-    }
-    
-    /// <summary>
-    /// 限速接口
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    public ResultModel<string> LimitApi()
-    {
-        return ResultModel<string>.Ok("LimitApi");
-    }
 }
