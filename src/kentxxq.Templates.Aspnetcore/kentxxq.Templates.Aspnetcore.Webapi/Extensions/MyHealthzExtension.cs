09f1c0717c8fc08e5e8c37be63ae5348211e2469
kentxxq <805429509@qq.com>
2022-12-16 20:24:23 +0800

    fix: 修正sqlite的健康检查

--- src/kentxxq.Templates.Aspnetcore/kentxxq.Templates.Aspnetcore.Webapi/Extensions/MyHealthzExtension.cs
+++ src/kentxxq.Templates.Aspnetcore/kentxxq.Templates.Aspnetcore.Webapi/Extensions/MyHealthzExtension.cs
@@ -27,9 +27,9 @@ public static class MyHealthzExtension
         service.AddHealthChecks()
 #if (EnableDB)
             // .AddMySql(configuration["Database:ConnectionString"] ?? throw new InvalidOperationException(),
-            //     "k_webapi", tags: new[] { "db" })
+            //     name:"k_webapi", tags: new[] { "db" })
             .AddSqlite(configuration["Database:ConnectionString"] ?? throw new InvalidOperationException(),
-                "k_webapi", tags: new[] { "db" })
+                name:"k_webapi", tags: new[] { "db" })
 #endif
             .AddCheck<StartupHealthz>("startup", tags: new[] { "k8s", "startup" })
             .AddCheck<LiveHealthz>("live", tags: new[] { "k8s", "live" });
